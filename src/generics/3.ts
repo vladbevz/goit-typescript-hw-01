ac9634ec761e5d37b454be6fe32422c5d1ce40fd
vbevz <122693446+vladbevz@users.noreply.github.com>
2024-10-09 21:13:17 +0200

    updates somes tasks

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,4 @@
-function merge<T, U>(objA: T, objB: U): T & U {
-    return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+    return Object.assign({}, objA, objB);
   }
   
