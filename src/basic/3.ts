9ec87cfa8552610f5cc80b22149f36751e69c2ef
vbevz <122693446+vladbevz@users.noreply.github.com>
2024-10-09 21:02:06 +0200

    initial comit

---
+++ src/basic/3.ts
@@ -0,0 +1,2 @@
+let mixed: string | number;
+let literal: 'enable' | 'disable';
---
+++ src/generics/4.ts
@@ -0,0 +1,16 @@
+type User = {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+  };
+  
+  function createOrUpdateUser(initialValues: Partial<User>) {
+    // Оновлення користувача
+  }
+  
+  createOrUpdateUser({
+    email: 'user@mail.com',
+    password: 'password123'
+  });
+  
